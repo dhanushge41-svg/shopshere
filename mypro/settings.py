e82bef0a94e32778cfd71e96d072e39db8d7ad06
dhanushge41-svg <dhanush.ge41@gmail.com>
2026-05-23 13:39:24 +0530

    Update settings.py

--- mypro/settings.py
+++ mypro/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoice.middleware.WhiteNoiceMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'mypro.urls'
